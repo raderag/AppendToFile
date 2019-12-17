b72ab78a933b948643e5c26a89b26116ad323b66
raderag <30390788+raderag@users.noreply.github.com>
2019-12-17 11:40:07 -0600

    initial commit

---
+++ FileAppend/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace FileAppend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+                return;
+
+            string fileName = args[0];
+
+            bool empty = false;
+
+            using (FileStream fs = (new FileStream(fileName, FileMode.Open)))
+            {
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    empty = br.ReadByte() == 0;
+                }
+
+                fs.Close();
+            }
+
+            using (StreamWriter sr = new StreamWriter(fileName, !empty))
+            {
+                sr.WriteLine("\n" + args[1]);
+                sr.Close();
+            }
+        }
+    }
+}
